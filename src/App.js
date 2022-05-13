3872b921c0878bf6cd77991a819b939bed439206
kimiaShahbaghi <kshahbaghi@gmail.com>
2022-05-13 15:27:44 +0430

    manage modal state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Carts/Cart";
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
     <>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,12 @@ import React from "react";
 import MealsImg from "../../assest/meals.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-function Header() {
+function Header(props) {
   return (
     <>
       <header className={classes.header}>
         <h1> ReactMelas</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={MealsImg} alt="table of food" />
